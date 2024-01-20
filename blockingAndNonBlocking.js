3412842ef76ddf5c4e7b583f048573c0c0af7c8c
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2024-01-20 20:28:50 +0530

    hello

---
+++ blockingAndNonBlocking.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+//Blocking --->> Synchronous
+console.log("1");
+const res = fs.readFileSync("./test.txt", "utf-8");
+console.log(res);
+console.log("2");
+console.log("3");
+
+//output
+
+// hey their
+// hey their
+// 1705760867573hey their
+// hey their
+// 1705760962967hey their
+// hey their
+// 1705761054390hey their
+// hey their
+// 1705761140987hey their
+
+// 2
+// 3
+
+/*-------Non blocking ------- */
+
+console.log("1");
+fs.readFile("./contect.txt", "utf-8", (err, res) => {
+  console.log(res);
+});
+
+console.log("2");
+console.log("3");
+
+
+// //output
+// 1
+// 2
+// 3
+// sushant: +912222222
