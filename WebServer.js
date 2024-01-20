b668e7a53e5b38d959ae7dfcd2d64552fe9fd893
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2024-01-20 21:09:34 +0530

    update

---
+++ WebServer.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} New Req Recived\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        res.end("I am sushat singh negi");
+        break;
+      default:
+        res.end("404 Note Found");
+    }
+  });
+  //   console.log(req);
+  //   console.log("New Req rec.");
+  //   res.end("Hello from Server");
+});
+
+myServer.listen(8000, () => console.log("Server is Running"));
